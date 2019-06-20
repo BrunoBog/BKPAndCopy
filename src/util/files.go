98666b5f57eb3b60e058db8f3602ff86a82138c0
brunobog <bog906@gmail.com>
2019-06-19 22:37:34 -0300

    clenup

--- src/util/files.go
+++ src/util/files.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"archive/zip"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -114,10 +113,6 @@ func copyDir(src string, dst string, orquestrador *sync.WaitGroup) (err error) {
 		if entry.IsDir() {
 			orquestrador.Add(1)
 			go copyDir(srcPath, dstPath, orquestrador)
-			// if err != nil {
-			// 	orquestrador.Done()
-			// 	return
-			// }
 		} else {
 			// Skip symlinks.
 			if entry.Mode()&os.ModeSymlink != 0 {
@@ -125,65 +120,8 @@ func copyDir(src string, dst string, orquestrador *sync.WaitGroup) (err error) {
 			}
 			orquestrador.Add(1)
 			go CopyFile(srcPath, dstPath, orquestrador)
-			// if err != nil {
-			// 	orquestrador.Done()
-			// 	return
-			// }
 		}
 	}
 	orquestrador.Done()
 	return
-}
-
-// ZipFiles compresses one or many files into a single zip archive file.
-// Param 1: filename is the output zip file's name.
-// Param 2: files is a list of files to add to the zip.
-func ZipFiles(filename string, files []string) error {
-
-	newZipFile, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer newZipFile.Close()
-
-	zipWriter := zip.NewWriter(newZipFile)
-	defer zipWriter.Close()
-
-	// Add files to zip
-	for _, file := range files {
-
-		zipfile, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-		defer zipfile.Close()
-
-		// Get the file information
-		info, err := zipfile.Stat()
-		if err != nil {
-			return err
-		}
-
-		header, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-
-		// Using FileInfoHeader() above only uses the basename of the file. If we want
-		// to preserve the folder structure we can overwrite this with the full path.
-		header.Name = file
-
-		// Change to deflate to gain better compression
-		// see http://golang.org/pkg/archive/zip/#pkg-constants
-		header.Method = zip.Deflate
-
-		writer, err := zipWriter.CreateHeader(header)
-		if err != nil {
-			return err
-		}
-		if _, err = io.Copy(writer, zipfile); err != nil {
-			return err
-		}
-	}
-	return nil
 }
